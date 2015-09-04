012b8674e14ec885031c468e04130addbbb561d3
Manuel Lehner <manuel.lehner@goyya.com>
2015-09-04 17:33:23 +0200

    fix compile error

--- android/src/com/goyya/telephonymanager/TelephonyManagerModule.java
+++ android/src/com/goyya/telephonymanager/TelephonyManagerModule.java
@@ -256,7 +256,6 @@ public class TelephonyManagerModule extends KrollModule
 	}
 	
 	@Kroll.method
-	@Kroll.setProperty
 	public boolean setPreferredNetworkTypeToGlobal(){
 		return getManager().setPreferredNetworkTypeToGlobal();
 	}
