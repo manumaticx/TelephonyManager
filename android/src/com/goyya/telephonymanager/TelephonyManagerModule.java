d45838518b3a41ec93b2c8d6f844fb065d27226d
Manuel Lehner <manuel.lehner@goyya.com>
2015-09-09 14:02:32 +0200

    expose missing constants

--- android/src/com/goyya/telephonymanager/TelephonyManagerModule.java
+++ android/src/com/goyya/telephonymanager/TelephonyManagerModule.java
@@ -69,7 +69,48 @@ public class TelephonyManagerModule extends KrollModule {
 	public static final int DATA_ACTIVITY_INOUT = TelephonyManager.DATA_ACTIVITY_INOUT;
 	@Kroll.constant
 	public static final int DATA_ACTIVITY_DORMANT = TelephonyManager.DATA_ACTIVITY_DORMANT;
+	@Kroll.constant
+	public static final int DATA_DISCONNECTED = TelephonyManager.DATA_DISCONNECTED;
+	@Kroll.constant
+	public static final int DATA_CONNECTING = TelephonyManager.DATA_CONNECTING;
+	@Kroll.constant
+	public static final int DATA_CONNECTED = TelephonyManager.DATA_CONNECTED;
+	@Kroll.constant
+	public static final int DATA_SUSPENDED = TelephonyManager.DATA_SUSPENDED;
 
+	@Kroll.constant
+	public static final int NETWORK_TYPE_1xRTT = TelephonyManager.NETWORK_TYPE_1xRTT;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_CDMA = TelephonyManager.NETWORK_TYPE_CDMA;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_EDGE = TelephonyManager.NETWORK_TYPE_EDGE;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_EHRPD = TelephonyManager.NETWORK_TYPE_EHRPD;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_EVDO_0 = TelephonyManager.NETWORK_TYPE_EVDO_0;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_EVDO_A = TelephonyManager.NETWORK_TYPE_EVDO_A;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_EVDO_B = TelephonyManager.NETWORK_TYPE_EVDO_B;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_GPRS = TelephonyManager.NETWORK_TYPE_GPRS;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_HSDPA = TelephonyManager.NETWORK_TYPE_HSDPA;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_HSPA = TelephonyManager.NETWORK_TYPE_HSPA;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_HSPAP = TelephonyManager.NETWORK_TYPE_HSPAP;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_HSUPA = TelephonyManager.NETWORK_TYPE_HSUPA;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_IDEN = TelephonyManager.NETWORK_TYPE_IDEN;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_LTE = TelephonyManager.NETWORK_TYPE_LTE;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_UMTS = TelephonyManager.NETWORK_TYPE_UMTS;
+	@Kroll.constant
+	public static final int NETWORK_TYPE_UNKNOWN = TelephonyManager.NETWORK_TYPE_UNKNOWN;
+	
 	@Kroll.constant
 	public static final int PHONE_TYPE_NONE = TelephonyManager.PHONE_TYPE_NONE;
 	@Kroll.constant
